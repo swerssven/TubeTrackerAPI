421a409485d90e65a8d7eab6b2e2b7b4b3d3dcb9
Sven Swers <swers_sven@hotmail.com>
2023-03-30 22:55:16 +0200

    Always show swagger

--- TubeTrackerAPI/Program.cs
+++ TubeTrackerAPI/Program.cs
@@ -53,11 +53,11 @@ builder.Services.AddCors(policyBuilder =>
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
+//if (app.Environment.IsDevelopment())
+//{
     app.UseSwagger();
     app.UseSwaggerUI();
-}
+//}
 
 app.UseCors();
 
